16e5330452fd48581dedbed3be703e466eedad5c
Mariana Chaves <79344040+marichaves@users.noreply.github.com>
2023-09-11 22:23:33 -0300

    Create Notas.java

---
+++ src/com/dominio/cursojava/exercicios/Notas.java
@@ -0,0 +1,43 @@
+package com.dominio.cursojava.exercicios;
+
+import java.util.Scanner;
+
+public class Notas {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		double[] notas1 = new double[10];
+		double[] notas2 = new double[notas1.length];
+		double[] resultados = new double[notas1.length];
+		
+		for (int i = 0; i< notas1.length; i++) {
+			System.out.println("Entre com a nota 1 do aluno" + (i + 1) +":");
+			notas1[i] = scan.nextDouble(); 
+			System.out.println("Entre com a nota 2 do aluno" + (i + 1) +":");
+			notas2[i] = scan.nextDouble(); 
+			
+			resultados[i] = (notas1[i] + notas2[i]) / 2;
+		}
+		
+		System.out.print("Notas 1: ");
+		for (int i = 0; i< notas1.length; i++) {
+			System.out.println(notas1[i] + " ");
+		}
+		System.out.println();
+		System.out.print("Notas 2: ");
+		for (int i = 0; i< notas2.length; i++) {
+			System.out.println(notas2[i] + " ");
+		}
+		System.out.println();
+		System.out.println("Resultados: ");
+		for (int i = 0; i< resultados.length; i++) {
+		
+			if(resultados[i] >= 7){
+				System.out.println(" - Aluno " + (i + 1 ) + " - " + resultados[i] + " - APROVADO");
+			}else {
+				System.out.println(" - Aluno " + (i + 1 ) + " - " + resultados[i] + " - REPROVADO");
+			}
+		}
+	}
+
+}
