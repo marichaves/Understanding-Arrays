949bfa3649c3b6b948e5fc673bf7bff8a15d37cc
Mariana Chaves <79344040+marichaves@users.noreply.github.com>
2023-09-15 17:27:59 -0300

    Create VetorAFormadoPelaPotenciaDeBaseDe2ElevadoAoExpoenteIgualAPosicaoDoRespectivoElemento.java

---
+++ src/com/dominio/cursojava/exercicios/VetorAFormadoPelaPotenciaDeBaseDe2ElevadoAoExpoenteIgualAPosicaoDoRespectivoElemento.java
@@ -0,0 +1,10 @@
+package com.dominio.cursojava.exercicios;
+
+public class VetorAFormadoPelaPotenciaDeBaseDe2ElevadoAoExpoenteIgualAPosicaoDoRespectivoElemento {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
