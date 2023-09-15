75441c976fcdc96c8aaf6a00d2752aacaf65e541
Mariana Chaves <79344040+marichaves@users.noreply.github.com>
2023-09-15 17:37:27 -0300

    Update VetorAFormadoPelaPotenciaDeBaseDe2ElevadoAoExpoenteIgualAPosicaoDoRespectivoElemento.java

--- src/com/dominio/cursojava/exercicios/VetorAFormadoPelaPotenciaDeBaseDe2ElevadoAoExpoenteIgualAPosicaoDoRespectivoElemento.java
+++ src/com/dominio/cursojava/exercicios/VetorAFormadoPelaPotenciaDeBaseDe2ElevadoAoExpoenteIgualAPosicaoDoRespectivoElemento.java
@@ -1,10 +1,22 @@
 package com.dominio.cursojava.exercicios;
 
+import java.util.Scanner;
+
 public class VetorAFormadoPelaPotenciaDeBaseDe2ElevadoAoExpoenteIgualAPosicaoDoRespectivoElemento {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 
 
+	        int[] vetorA = new int[11];
+
+	        for (int i = 0; i < vetorA.length; i++) {
+	       
+	            vetorA[i] = (int) Math.pow(2, i);
+	        }
+	        System.out.print("Valores do vetor A = ");
+			for (int i = 0; i < vetorA.length; i++) {
+				System.out.print(vetorA[i] + " ");
+			}
 	}
 
 }
